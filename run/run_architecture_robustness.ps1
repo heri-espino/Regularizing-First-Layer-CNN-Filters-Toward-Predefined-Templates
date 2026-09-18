706568bdff9ed9c21df4214f6709214160b314fc
Heriberto Espino Montelongo <heriberto.espinomo@udlap.mx>
2026-09-18 13:53:03 -0600

    stage F: make wrapper forward all PowerShell arguments

--- run/run_architecture_robustness.ps1
+++ run/run_architecture_robustness.ps1
@@ -1,8 +1,5 @@
-param(
-    [Parameter(ValueFromRemainingArguments=$true)]
-    [object[]]$RemainingArgs
-)
-
 $script = Join-Path $PSScriptRoot '..\studies\cnn_architecture_robustness\run_architecture_robustness.ps1'
-& $script @RemainingArgs
-exit $LASTEXITCODE
+& $script @args
+if ($null -ne $LASTEXITCODE) {
+    exit $LASTEXITCODE
+}
